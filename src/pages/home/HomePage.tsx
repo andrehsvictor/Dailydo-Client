b6c7e11428e0e85e950ba0b0823892830b387ab8
André Victor <andrehsvictor@gmail.com>
2023-12-26 20:28:33 -0300

    feat: created HomePage component

---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
+import LocalStorageKeys from "../../utils/LocalStorageKeys";
+import Task from "../../utils/Types";
+
+const HomePage = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  function getTitle(event: React.ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value);
+  }
+
+  function getDescription(event: React.ChangeEvent<HTMLInputElement>) {
+    setDescription(event.target.value);
+  }
+
+  async function handleClick(
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) {
+    const request = await fetch(
+      "https://dailydo-api.onrender.com/task/create",
+      {
+        method: "POST",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(
+            LocalStorageKeys.ACCESS_TOKEN
+          )}`,
+          "content-type": "application/json",
+        },
+      }
+    );
+    const response = await request.json();
+
+    if (request.status != 201) {
+      let errorMessage = {
+        message: "",
+      };
+      errorMessage = response;
+      alert("Error: " + errorMessage.message);
+    }
+  }
+
+  useEffect(() => {
+    fetch("https://dailydo-api.onrender.com/tasks", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(
+          LocalStorageKeys.ACCESS_TOKEN
+        )}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => setTasks(response));
+  }, [tasks]);
+
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-3xl mx-auto px-4 py-10">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">Tarefas</h1>
+        <TaskInput
+          onTitleChange={getTitle}
+          onDescriptionChange={getDescription}
+          titleValue={title}
+          descriptionValue={description}
+          onButtonClick={handleClick}
+        />
+        <TaskList items={tasks} />
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
