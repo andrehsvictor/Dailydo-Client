010217103cee2b4ea829747a352f488274ebcff4
André Victor <andrehsvictor@gmail.com>
2023-12-26 20:30:29 -0300

    feat: created utiliy components for the project

---
+++ src/utils/LocalStorageKeys.tsx
@@ -0,0 +1,7 @@
+enum LocalStorageKeys {
+  ACCESS_TOKEN = "@Dailydo: access_token",
+  REFRESH_TOKEN = "@Dailydo: refresh_token",
+  EXPIRATION = "@Dailydo: expiration",
+}
+
+export default LocalStorageKeys;
---
+++ src/utils/Types.tsx
@@ -0,0 +1,9 @@
+type Task = {
+  id: number | null;
+  title: string | null;
+  description: string | null;
+  createdAt: Date | null;
+  expiresAt: Date | null;
+};
+
+export default Task;
