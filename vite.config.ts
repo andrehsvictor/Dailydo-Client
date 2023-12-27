2055427b9f07e358cb010b264ddc7bf1017ea477
André Victor <andrehsvictor@gmail.com>
2023-12-27 09:31:13 -0300

    feat: added some configurations for vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
     port: 80,
+    host: true,
+    strictPort: true,
+    origin: "http://0.0.0.0:80",
   },
 });
